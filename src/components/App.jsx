03f0bbb45649df29450110c03a0378be656853a3
Rew <irinavakulenko2000@gmail.com>
2023-05-14 23:53:42 +0300

    10001

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,33 @@ export class App extends Component {
       { id: nanoid(), text: 'Survive a Redux', completed: false }
     ]
   }
+  toggleCompeted = todoIid => {
+    console.log(todoIid);
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoIid) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed
+    //       };
+    //     }
+    //     return todo;
+    //   })
+    // })
+
+    // )
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(
+        todo => todo.id === todoIid ? { ...todo, completed: !todo.cpmpleted } : todo,
+      ),
+    }));
+  };
   
+
       handleDelete = (todoIid) => {       
         this.setState(prevState => ({
-            todos: prevState.todos.filter(todo => todo.is !== todoIid),
+            todos: prevState.todos.filter(todo => todo.id !== todoIid),
         })
             
         
@@ -38,6 +61,7 @@ export class App extends Component {
         <h1>State Component</h1>
         <ToDoList
           onDeleteToDo={this.handleDelete}
+          onHandleCompleted={this.toggleCompeted}
           todos = {todos}
         />
     </div>
--- src/components/ToDoList/toDoList.jsx
+++ src/components/ToDoList/toDoList.jsx
@@ -11,41 +11,49 @@ export class ToDoList extends Component {
     }
 
     handleChange = evt => {
+    // const { todos } = this.props;
     const { name, value, type, checked } = evt.target;
     // Якщо тип елемента – checkbox, беремо значення checked,
     // в іншому випадку – value
-        console.log(evt);
+        console.log(evt.target.value);
         this.setState({
             [name]: type === "checkbox" ? checked : value,
-            // [name]: disabled === { agreed },
-        
-        });
-    };
+            agreed: checked,
+        })
+    }
+    
 
 
     
     render() {
-        const { todos, onDeleteToDo } = this.props;
+        const { todos, onDeleteToDo, onHandleCompleted } = this.props;
+        const { agreed } = this.state;
 
-            return(
+        return (
+            <>
+                <div>
+                    Total todo: {todos.length}
+                </div>
         <List>
-        {todos.map(({ id, text, agreed }) => (
+        {todos.map(({ id, text, completed }) => (
             <li className='' key={id}>
             
             <p>{text}</p>
 
                 <input
                     type="checkbox"
-                    name='checkbox'
-                    checked={agreed}
-                    onClick={this.handleChange} />
+                    // name='checkbox'
+                    checked={completed}
+                    // value={id}
+                    onChange={() => (onHandleCompleted(id))} />
            
-        <Button type="button" onClick={() => onDeleteToDo(id)}>Delete</Button>
+        <Button type="button" disabled={!completed} onClick={() => onDeleteToDo(id)}>Delete</Button>
     
             </li>
             ))
         }
-            </List>
+                </List>
+                </>
             )}
     }
     
